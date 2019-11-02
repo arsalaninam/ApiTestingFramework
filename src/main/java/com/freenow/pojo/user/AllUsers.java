033bc65b8aef51a7373117fb6187e14bdd5323c9
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-02 13:27:38 +0500

    Add Users response pojo

--- src/main/java/com/freenow/pojo/user/AllUsers.java
+++ src/main/java/com/freenow/pojo/user/AllUsers.java
@@ -1,4 +1,18 @@
 package com.freenow.pojo.user;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class AllUsers {
+
+    List<SingleUser> listOfUsers;
+
+    @Override
+    public String toString() {
+        return "AllUsers [listOfUsers=" + listOfUsers + "]";
+    }
 }
--- src/main/java/com/freenow/pojo/user/SingleUser.java
+++ src/main/java/com/freenow/pojo/user/SingleUser.java
@@ -1,4 +1,25 @@
 package com.freenow.pojo.user;
 
+import com.freenow.pojo.address.Address;
+import com.freenow.pojo.company.Company;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SingleUser {
+
+    private int id;
+    private String name;
+    private String username;
+    private String email;
+    private Address address;
+    private String phone;
+    private String website;
+    private Company company;
+
+    @Override
+    public String toString() {
+        return "ClassPojo [id = " + id + ", name = " + name + ", username = " + username + ", email = " + email + ", address = " + address + ", phone = " + phone + ", website = " + website + ", company = " + company + "]";
+    }
 }
