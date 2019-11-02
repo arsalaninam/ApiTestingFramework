61b6b58d5fd5d9904ab7865cca0ae9a5abb7d6a4
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-02 20:20:45 +0500

    Add basic test cases to validate comments endpoint

--- src/test/java/com/freenow/testcase/singleEndpoint/comment/CommentsTest.java
+++ src/test/java/com/freenow/testcase/singleEndpoint/comment/CommentsTest.java
@@ -1,4 +1,107 @@
 package com.freenow.testcase.singleEndpoint.comment;
 
-public class CommentsTest {
+import com.freenow.businesslayer.comment.SingleCommentBusinessLogic;
+import com.freenow.data.dataprovider.CommentDataProvider;
+import com.freenow.data.dataprovider.CommonDataProvider;
+import com.freenow.pojo.comment.SingleComment;
+import com.freenow.testcase.singleEndpoint.SingleEndpointCommon;
+import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static com.freenow.constant.ResponseCodeConstant.STATUS_CODE_404;
+import static com.freenow.constant.ScenarioNameConstant.*;
+import static com.freenow.constant.ServiceConstant.COMMENTS_ENDPOINT;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+
+public class CommentsTest extends SingleEndpointCommon {
+
+    private static final Logger log = LoggerFactory.getLogger(CommentsTest.class);
+    private String commentsEndpoint = prop.getProperty(COMMENTS_ENDPOINT);
+
+    /**********************************************************
+     * Send a GET request to /comments. Validate that response
+     * has HTTP status code 200 and Content Type JSON
+     *********************************************************/
+    @Test
+    public void testResponseStatusCode200AndContentTypeJSON() {
+        log.info(VALIDATE_STATUS_CODE_200_AND_CONTENT_TYPE_JSON + commentsEndpoint);
+        given().
+                spec(requestSpecification).
+                when().
+                get(commentsEndpoint).
+                then().
+                spec(responseSpecification);
+    }
+
+    /***********************************************************
+     * Send a GET request to /comments. Validate that
+     * - response body contains list of 500 comments
+     * - response has HTTP status code 200 and Content Type JSON
+     ***********************************************************/
+    @Test(dependsOnMethods = "testResponseStatusCode200AndContentTypeJSON")
+    public void fetchListOfCommentsAndAssertSize() {
+        log.info(VALIDATE_LIST_OF_ITEM + commentsEndpoint);
+        given().
+                spec(requestSpecification).
+                when().
+                get(commentsEndpoint).
+                then().
+                assertThat().
+                body("$", hasSize(500)).
+                and().
+                spec(responseSpecification);
+    }
+
+    /************************************************************
+     * Send a GET request to /comments/{commentsId} and Validate
+     * - response has HTTP status code 200 & Content Type is JSON
+     ***********************************************************/
+    @Test(dataProvider = "validId", dataProviderClass = CommonDataProvider.class)
+    public void testResponseCodeAndContentType(int id) {
+        log.info(VALIDATE_STATUS_CODE_200_AND_CONTENT_TYPE_JSON + commentsEndpoint + id);
+        given().
+                spec(requestSpecification).
+                when().
+                get(commentsEndpoint + id).
+                then().
+                spec(responseSpecification);
+    }
+
+    /************************************************************
+     * Send a GET request to /comments/{commentsId} and Validate
+     * - response returns the expected id, name, email & body
+     ***********************************************************/
+    @Test(dependsOnMethods = {"testResponseCodeAndContentType"},
+            dataProvider = "validCommentsIdWithNameAndEmail",
+            dataProviderClass = CommentDataProvider.class)
+    public void testResponseBodyWithIdNameAndEmail(int id, String name, String email) {
+        log.info(VALIDATE_RESPONSE_BODY + commentsEndpoint + id);
+
+        SingleComment singleComment = SingleCommentBusinessLogic.getSingleCommentById(id);
+
+        softAssert.assertEquals(singleComment.getId(), id);
+        softAssert.assertEquals(singleComment.getName(), name);
+        softAssert.assertEquals(singleComment.getEmail(), email);
+        softAssert.assertNotNull(singleComment.getBody());
+        softAssert.assertAll();
+    }
+
+    /***********************************************************************
+     * Send a GET request to /comments/{commentsId} with invalid commentsId
+     * Validate that response has HTTP status code 404
+     **********************************************************************/
+    @Test(dataProvider = "invalidId", dataProviderClass = CommonDataProvider.class)
+    public void testResponseCodeWithInvalidCommentsId(int id) {
+        log.info(VALIDATE_STATUS_CODE_404 + commentsEndpoint + id);
+        Response response = given().
+                spec(requestSpecification).
+                when().
+                get(commentsEndpoint + id);
+        Assert.assertEquals(response.statusCode(), STATUS_CODE_404);
+    }
+
 }
