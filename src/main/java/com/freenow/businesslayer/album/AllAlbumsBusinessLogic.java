34ce978ded8b071e28c3734dc4709f1d13bd452e
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-02 16:10:01 +0500

    Write business logic implementations to fetch response of albums

--- src/main/java/com/freenow/businesslayer/album/AllAlbumsBusinessLogic.java
+++ src/main/java/com/freenow/businesslayer/album/AllAlbumsBusinessLogic.java
@@ -1,4 +1,55 @@
 package com.freenow.businesslayer.album;
 
-public class AllAlbumsBusinessLogic {
+import com.freenow.pojo.album.AllAlbums;
+import com.freenow.pojo.album.SingleAlbum;
+import com.freenow.util.PropertyReader;
+import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.freenow.constant.ServiceConstant.*;
+import static io.restassured.RestAssured.when;
+
+public class AllAlbumsBusinessLogic extends PropertyReader {
+
+    private static final Logger log = LoggerFactory.getLogger(AllAlbumsBusinessLogic.class);
+
+    /**
+     * Fetch list of all albums
+     * e.g. https://jsonplaceholder.typicode.com/albums
+     */
+    public static AllAlbums getAllAlbums() {
+        String baseUrl = prop.getProperty(BASE_URL);
+        String albumsEndpoint = prop.getProperty(ALBUMS_ENDPOINT);
+        String url = baseUrl + albumsEndpoint;
+        log.info("URL to be hit:" + url);
+
+        Response response = when().get(url);
+        List<SingleAlbum> allSingleAlbums = Arrays.asList(response.getBody().as(SingleAlbum[].class));
+
+        AllAlbums allAlbums = new AllAlbums();
+        allAlbums.setListOfAlbums(allSingleAlbums);
+        return allAlbums;
+    }
+
+    /**
+     * Fetch list of all albums json set respective to particular userId
+     * e.g. https://jsonplaceholder.typicode.com/albums?userId=1
+     */
+    public static AllAlbums getAllAlbumsForUserId(String userId) {
+        String baseUrl = prop.getProperty(BASE_URL);
+        String albumsEndpointQueryParamUserId = prop.getProperty(ALBUMS_ENDPOINT_QUERY_PARAM_USER_ID);
+        String url = baseUrl + albumsEndpointQueryParamUserId + userId;
+        log.info("URL to be hit:" + url);
+
+        Response response = when().get(url);
+        List<SingleAlbum> allSingleAlbums = Arrays.asList(response.getBody().as(SingleAlbum[].class));
+
+        AllAlbums allAlbums = new AllAlbums();
+        allAlbums.setListOfAlbums(allSingleAlbums);
+        return allAlbums;
+    }
 }
