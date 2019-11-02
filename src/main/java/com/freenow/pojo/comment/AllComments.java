3430957c89db070894826ab2be13309104260e30
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-02 13:26:07 +0500

    Add comments response pojo

--- src/main/java/com/freenow/pojo/comment/AllComments.java
+++ src/main/java/com/freenow/pojo/comment/AllComments.java
@@ -1,4 +1,18 @@
 package com.freenow.pojo.comment;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class AllComments {
+
+    List<SingleComment> listOfComments;
+
+    @Override
+    public String toString() {
+        return "AllComments [listOfComments=" + listOfComments + "]";
+    }
 }
--- src/main/java/com/freenow/pojo/comment/SingleComment.java
+++ src/main/java/com/freenow/pojo/comment/SingleComment.java
@@ -1,4 +1,20 @@
 package com.freenow.pojo.comment;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SingleComment {
+
+    private int postId;
+    private int id;
+    private String name;
+    private String email;
+    private String body;
+
+    @Override
+    public String toString() {
+        return "ClassPojo [postId = " + postId + ", id = " + id + ", name = " + name + ", email = " + email + ", body = " + body + "]";
+    }
 }
