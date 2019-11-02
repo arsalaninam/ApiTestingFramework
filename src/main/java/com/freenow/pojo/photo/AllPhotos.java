a8d454cb730e043194be815dcf9b89d04e53f485
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-02 13:26:37 +0500

    Add photos response pojo

--- src/main/java/com/freenow/pojo/photo/AllPhotos.java
+++ src/main/java/com/freenow/pojo/photo/AllPhotos.java
@@ -1,4 +1,18 @@
 package com.freenow.pojo.photo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class AllPhotos {
+
+    List<SinglePhoto> listOfPhotos;
+
+    @Override
+    public String toString() {
+        return "AllPhotos [listOfPhotos=" + listOfPhotos + "]";
+    }
 }
--- src/main/java/com/freenow/pojo/photo/SinglePhoto.java
+++ src/main/java/com/freenow/pojo/photo/SinglePhoto.java
@@ -1,4 +1,20 @@
 package com.freenow.pojo.photo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SinglePhoto {
+
+    private int albumId;
+    private int id;
+    private String title;
+    private String url;
+    private String thumbnailUrl;
+
+    @Override
+    public String toString() {
+        return "ClassPojo [albumId = " + albumId + ", id = " + id + ", title = " + title + ", url = " + url + ", thumbnailUrl = " + thumbnailUrl + "]";
+    }
 }
