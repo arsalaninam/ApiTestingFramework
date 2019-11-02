6d0302296adf9200bef55f314b66b209b765bfa6
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-02 16:10:57 +0500

    Write business logic implementations to fetch response of users

--- src/main/java/com/freenow/businesslayer/user/SingleUserBusinessLogic.java
+++ src/main/java/com/freenow/businesslayer/user/SingleUserBusinessLogic.java
@@ -1,4 +1,34 @@
 package com.freenow.businesslayer.user;
 
-public class SingleUserBusinessLogic {
+import com.freenow.pojo.user.SingleUser;
+import com.freenow.util.PropertyReader;
+import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.freenow.constant.ServiceConstant.BASE_URL;
+import static com.freenow.constant.ServiceConstant.USERS_ENDPOINT;
+import static io.restassured.RestAssured.when;
+
+public class SingleUserBusinessLogic extends PropertyReader {
+
+    private static final Logger log = LoggerFactory.getLogger(SingleUserBusinessLogic.class);
+
+    /**
+     * Extract data as Response object
+     * Populate Single User Response POJOs
+     * e.g. https://jsonplaceholder.typicode.com/users/1
+     *
+     * @param id - user id to fetch a single user
+     */
+    public static SingleUser getSingleUserById(String id) {
+        String baseUrl = prop.getProperty(BASE_URL);
+        String usersEndpoint = prop.getProperty(USERS_ENDPOINT);
+        String url = baseUrl + usersEndpoint + id;
+        log.info("API to be hit:" + url);
+
+        Response response = when().get(url);
+        SingleUser singleUser = response.getBody().as(SingleUser.class);
+        return singleUser;
+    }
 }
