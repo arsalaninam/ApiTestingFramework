8793dd13293ef2338057eee41da8d92ab8fea125
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-02 13:26:49 +0500

    Add post response pojo

--- src/main/java/com/freenow/pojo/post/AllPosts.java
+++ src/main/java/com/freenow/pojo/post/AllPosts.java
@@ -1,4 +1,18 @@
 package com.freenow.pojo.post;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class AllPosts {
+
+    List<SinglePost> listOfPosts;
+
+    @Override
+    public String toString() {
+        return "AllPosts [listOfPosts=" + listOfPosts + "]";
+    }
 }
--- src/main/java/com/freenow/pojo/post/SinglePost.java
+++ src/main/java/com/freenow/pojo/post/SinglePost.java
@@ -1,4 +1,19 @@
 package com.freenow.pojo.post;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SinglePost {
+
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+
+    @Override
+    public String toString() {
+        return "ClassPojo [userId = " + userId + ", id = " + id + ", title = " + title + ", body = " + body + "]";
+    }
 }
