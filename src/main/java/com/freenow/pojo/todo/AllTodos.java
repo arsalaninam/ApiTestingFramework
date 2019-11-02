7e1544a85617d87e16ff5c9555a5f4d20ee9eb37
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-02 13:27:18 +0500

    Add todo response pojo

--- src/main/java/com/freenow/pojo/todo/AllTodos.java
+++ src/main/java/com/freenow/pojo/todo/AllTodos.java
@@ -1,4 +1,18 @@
 package com.freenow.pojo.todo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class AllTodos {
+
+    List<SingleTodo> listOfTodos;
+
+    @Override
+    public String toString() {
+        return "AllTodos [listOfTodos=" + listOfTodos + "]";
+    }
 }
--- src/main/java/com/freenow/pojo/todo/SingleTodo.java
+++ src/main/java/com/freenow/pojo/todo/SingleTodo.java
@@ -1,4 +1,19 @@
 package com.freenow.pojo.todo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SingleTodo {
+
+    private int userId;
+    private int id;
+    private String title;
+    private String completed;
+
+    @Override
+    public String toString() {
+        return "ClassPojo [userId = " + userId + ", id = " + id + ", title = " + title + ", completed = " + completed + "]";
+    }
 }
