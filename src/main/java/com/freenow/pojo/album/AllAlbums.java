2726630a771a8b09ebcb280705f8eec6f1ede438
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-02 13:25:49 +0500

    Add album response pojo

--- src/main/java/com/freenow/pojo/album/AllAlbums.java
+++ src/main/java/com/freenow/pojo/album/AllAlbums.java
@@ -1,4 +1,18 @@
 package com.freenow.pojo.album;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class AllAlbums {
+
+    List<SingleAlbum> listOfAlbums;
+
+    @Override
+    public String toString() {
+        return "AllAlbums [listOfAlbums=" + listOfAlbums + "]";
+    }
 }
--- src/main/java/com/freenow/pojo/album/SingleAlbum.java
+++ src/main/java/com/freenow/pojo/album/SingleAlbum.java
@@ -1,4 +1,18 @@
 package com.freenow.pojo.album;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SingleAlbum {
+
+    private int userId;
+    private int id;
+    private String title;
+
+    @Override
+    public String toString() {
+        return "ClassPojo [userId = " + userId + ", id = " + id + ", title = " + title + "]";
+    }
 }
