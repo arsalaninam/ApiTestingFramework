68c07e23dce9121dc5d980c3305add71ecf026df
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-02 16:10:25 +0500

    Write business logic implementations to fetch response of photos

--- src/main/java/com/freenow/businesslayer/photo/AllPhotosBusinessLogic.java
+++ src/main/java/com/freenow/businesslayer/photo/AllPhotosBusinessLogic.java
@@ -1,4 +1,55 @@
 package com.freenow.businesslayer.photo;
 
-public class AllPhotosBusinessLogic {
+import com.freenow.pojo.photo.AllPhotos;
+import com.freenow.pojo.photo.SinglePhoto;
+import com.freenow.util.PropertyReader;
+import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.freenow.constant.ServiceConstant.*;
+import static io.restassured.RestAssured.when;
+
+public class AllPhotosBusinessLogic extends PropertyReader {
+
+    private static final Logger log = LoggerFactory.getLogger(AllPhotosBusinessLogic.class);
+
+    /**
+     * Fetch list of all photos
+     * e.g. https://jsonplaceholder.typicode.com/photos
+     */
+    public static AllPhotos getAllPhotos() {
+        String baseUrl = prop.getProperty(BASE_URL);
+        String photosEndpoint = prop.getProperty(PHOTOS_ENDPOINT);
+        String url = baseUrl + photosEndpoint;
+        log.info("URL to be hit:" + url);
+
+        Response response = when().get(url);
+        List<SinglePhoto> allSinglePhotos = Arrays.asList(response.getBody().as(SinglePhoto[].class));
+
+        AllPhotos allPhotos = new AllPhotos();
+        allPhotos.setListOfPhotos(allSinglePhotos);
+        return allPhotos;
+    }
+
+    /**
+     * Fetch list of all photos json set respective to particular albumId
+     * e.g. https://jsonplaceholder.typicode.com/photos?albumId=1
+     */
+    public static AllPhotos getAllPhotosForAlbumId(String albumId) {
+        String baseUrl = prop.getProperty(BASE_URL);
+        String photosEndpointQueryParamAlbumId = prop.getProperty(PHOTOS_ENDPOINT_QUERY_PARAM_ALBUM_ID);
+        String url = baseUrl + photosEndpointQueryParamAlbumId + albumId;
+        log.info("URL to be hit:" + url);
+
+        Response response = when().get(url);
+        List<SinglePhoto> allSinglePhotos = Arrays.asList(response.getBody().as(SinglePhoto[].class));
+
+        AllPhotos allPhotos = new AllPhotos();
+        allPhotos.setListOfPhotos(allSinglePhotos);
+        return allPhotos;
+    }
 }
--- src/main/java/com/freenow/businesslayer/photo/SinglePhotoBusinessLogic.java
+++ src/main/java/com/freenow/businesslayer/photo/SinglePhotoBusinessLogic.java
@@ -1,4 +1,34 @@
 package com.freenow.businesslayer.photo;
 
-public class SinglePhotoBusinessLogic {
+import com.freenow.pojo.photo.SinglePhoto;
+import com.freenow.util.PropertyReader;
+import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.freenow.constant.ServiceConstant.BASE_URL;
+import static com.freenow.constant.ServiceConstant.PHOTOS_ENDPOINT;
+import static io.restassured.RestAssured.when;
+
+public class SinglePhotoBusinessLogic extends PropertyReader {
+
+    private static final Logger log = LoggerFactory.getLogger(SinglePhotoBusinessLogic.class);
+
+    /**
+     * Extract data as Response object
+     * Populate Single Photo Response POJOs
+     * https://jsonplaceholder.typicode.com/photos/1
+     *
+     * @param id - photo id to fetch a single photo
+     */
+    public static SinglePhoto getSinglePhotoById(String id) {
+        String baseUrl = prop.getProperty(BASE_URL);
+        String photosEndpoint = prop.getProperty(PHOTOS_ENDPOINT);
+        String url = baseUrl + photosEndpoint + id;
+        log.info("API to be hit:" + url);
+
+        Response response = when().get(url);
+        SinglePhoto singlePhoto = response.getBody().as(SinglePhoto.class);
+        return singlePhoto;
+    }
 }
