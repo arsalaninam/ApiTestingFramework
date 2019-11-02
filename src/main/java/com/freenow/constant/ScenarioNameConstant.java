cdf41288000cf8d573acea730f703484bdedd38f
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-02 19:50:17 +0500

    Add scnario names consants to use for logging

---
+++ src/main/java/com/freenow/constant/ScenarioNameConstant.java
@@ -0,0 +1,11 @@
+package com.freenow.constant;
+
+public interface ScenarioNameConstant {
+
+    String VALIDATE_STATUS_CODE_200_AND_CONTENT_TYPE_JSON =
+            "Validate Response Status Code is 200 and Content Type is JSON of API : ";
+
+    String VALIDATE_STATUS_CODE_404 = "Validate Response Status Code is 404 of API : ";
+    String VALIDATE_LIST_OF_ITEM = "Validate List of Items in API : ";
+    String VALIDATE_RESPONSE_BODY = "Validate Response Body of API : ";
+}
