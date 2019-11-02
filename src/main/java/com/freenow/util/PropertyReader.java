af5ed7ba2f43bfb7c635fddcfc77bb6211e364d0
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-02 16:08:35 +0500

    Add Property Reader class to read application properties

--- src/main/java/com/freenow/util/PropertyReader.java
+++ src/main/java/com/freenow/util/PropertyReader.java
@@ -1,4 +1,21 @@
 package com.freenow.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertyReader {
+
+    protected static Properties prop;
+
+    public PropertyReader() {
+        try {
+            prop = new Properties();
+            FileInputStream ip = new FileInputStream(System.getProperty("user.dir") + "/src/main/resources"
+                    + "/config/config.properties");
+            prop.load(ip);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
