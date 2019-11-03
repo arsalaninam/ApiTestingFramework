79df95bbfe0f2ebe95998d5c5ca1dbe2a37c9f53
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-03 19:25:24 +0500

    Add required test data in Common Data Provider

--- src/main/java/com/freenow/data/dataprovider/CommonDataProvider.java
+++ src/main/java/com/freenow/data/dataprovider/CommonDataProvider.java
@@ -3,7 +3,7 @@ package com.freenow.data.dataprovider;
 import org.testng.annotations.DataProvider;
 
 /**
- * A common data provider class to provide similar test data
+ * A common data provider class to provide test data used in multiple test cases
  *
  * @author Arsalan Inam
  */
@@ -20,6 +20,27 @@ public class CommonDataProvider {
     public static Object[][] invalidId() {
         return new Object[][]{
                 {7000}, {8000}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] validUsernameWithNumberOfAlbums() {
+        return new Object[][]{
+                {"Bret", 10}, {"Samantha", 10}, {"Maxime_Nienow", 10}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] validUsernameWithNumberOfPosts() {
+        return new Object[][]{
+                {"Antonette", 10}, {"Karianne", 10}, {"Kamren", 10}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] validUsernameWithNumberOfTodos() {
+        return new Object[][]{
+                {"Leopoldo_Corkery", 20}, {"Elwyn.Skiles", 20}, {"Moriah.Stanton", 20}
         };
     }
 }
